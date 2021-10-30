87f3b44e478f7c5bc818c834107d15fdfb85774e
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-30 15:37:53 +0100

    fixed motion for links in burger menu

--- src/Components/NavBar/NavLinks.js
+++ src/Components/NavBar/NavLinks.js
@@ -2,52 +2,82 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const NavLinks = (props) => {
-  const animateFrom = { opacity: 0, y: -40 };
-  const animateTo = { opacity: 1, y: 0 };
+  // const animateFrom = { opacity: 0, y: -80 };
+  // const animateTo = { opacity: 1, y: 0 };
   return (
-    <ul>
+    <motion.ul
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <motion.li
-        intial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.05 }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 10,
+        }}
+        transition={{ delay: 0.1 }}
         onClick={() => {
           props.isMobile && props.closeMobileMenu();
         }}
       >
         <a href="#about-container">About</a>
       </motion.li>
-      
+
       <motion.li
-        intial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.1 }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 15,
+        }}
+        transition={{ delay: 0.2 }}
         onClick={() => {
           props.isMobile && props.closeMobileMenu();
         }}
       >
         <a href="#experience-container">Experience</a>
       </motion.li>
-      <li
-        intial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.2 }}
+
+      <motion.li
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 15,
+        }}
+        transition={{ delay: 0.3 }}
         onClick={() => {
           props.isMobile && props.closeMobileMenu();
         }}
       >
         <a href="#project-container">Projects</a>
-      </li>
+      </motion.li>
+
       <motion.li
-        intial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.3 }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 15,
+        }}
+        transition={{ delay: 0.4 }}
         onClick={() => {
           props.isMobile && props.closeMobileMenu();
         }}
       >
         <a href="#contact-container">Contact</a>
       </motion.li>
-    </ul>
+    </motion.ul>
   );
 };
 
