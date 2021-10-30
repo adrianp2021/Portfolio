7846498deedc2c617467bbde6cab8d83080e9315
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-30 17:36:06 +0100

    changed style for burger links

--- src/Components/NavBar/NavLinks.js
+++ src/Components/NavBar/NavLinks.js
@@ -6,7 +6,7 @@ const NavLinks = (props) => {
     <motion.ul
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.1 }}
+      transition={{ duration: 0.1, ease: "easeOut" }}
       exit={{ opacity: 0 }}
     >
       <motion.li
